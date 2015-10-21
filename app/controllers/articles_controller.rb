3caaa65df87469de2141bbda0a4cbe959c383d75
GabeKagan <gabekagan@live.com>
2015-10-21 15:06:41 -0400

    5.11 - Editing articles
    Some of the more prominent placeholders have been... improved?

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,23 @@ class ArticlesController < ApplicationController
         render 'new'
       end
     end
+
+    def edit
+      @article = Article.find(params[:id])
+    end
      
     def show
         @article = Article.find(params[:id])
+    end
+
+    def update
+      @article = Article.find(params[:id])
+     
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
     end
 
     def index
