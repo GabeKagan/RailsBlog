573f832adde26034a9b96413055669d0d03504f9
GabeKagan <gabekagan@live.com>
2015-10-24 16:15:09 -0400

    The rest of unit 6
    These define the comment system, which is apparently nested into the
    article system.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   resources :articles
+  
+  resources :articles do
+    resources :comments
+  end
 
   root 'welcome#index'
 
