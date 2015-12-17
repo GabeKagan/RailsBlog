2024e6e84244f2814baab7aaf6d952d7c535efcd
GabeKagan <gabekagan@live.com>
2015-12-17 13:51:27 -0500

    Very basic comment CAPTCHA
    This randomly generates a simple math problem that the user must answer
    correctly in order to post a comment. Problems include the use of global
    variables, insufficient frequency number of randomization, and a lack of
    feedback when the user screws up or is a bot. I should work on those at
    some point.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 A quick "Hello World" type program using Rails 4.1.8 and its blog template. Cool, innerit?
 I'm going to try and pick up as much as I can by expanding it and working through the documentation at http://guides.rubyonrails.org/v4.1.8/getting_started.html.
 It might be best to use a slightly newer version of Rails, but I don't expect enormous changes.
+
+Next task: Make a "captcha" for comments based on addition questions.
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,16 @@
 class CommentsController < ApplicationController
   
   http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+    $int1 = 1 + rand(9)
+    $int2 = 1 + rand(9)
 
   def create
     @article = Article.find(params[:article_id])
-    @comment = @article.comments.create(comment_params)
+    @result = params[:answer] #Gets passed properly.
+    #render :text => $int1
+    if ($int1).to_i + ($int2).to_i == (@result).to_i
+      @comment = @article.comments.create(comment_params)
+    end
     redirect_to article_path(@article)
   end
  
